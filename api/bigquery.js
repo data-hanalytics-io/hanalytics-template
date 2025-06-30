28f5b1229a60de909fb37545a72db083d061628a
Marie-Grace OKRY <marie-grace@data-hanalytics.io>
2025-06-30 14:59:23 +0200

    modif deploiement

--- api/bigquery.js
+++ api/bigquery.js
@@ -1,6 +1,9 @@
 const { BigQuery } = require('@google-cloud/bigquery');
 const path = require('path');
-require('dotenv').config({ path: path.resolve(__dirname, '../.env') });
+if (process.env.NODE_ENV === 'development') {
+  require('dotenv').config({ path: path.resolve(__dirname, '../.env') });
+}
+
 
 // Gestion des credentials (local ou production)
 function getCredentials() {
